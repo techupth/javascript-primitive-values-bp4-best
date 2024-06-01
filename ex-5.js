ec50fe5b9385a38df2d796bd6f718df4683da9bf
Thammalak <thammalak.ti@gmail.com>
2024-06-01 20:07:05 +0700

    Assingment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let totalPrice = 27000;
+let disCount = 25 * totalPrice / 100 ;
+let priceAmount = totalPrice - disCount;
+console.log(priceAmount)
+
